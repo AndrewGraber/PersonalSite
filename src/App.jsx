3a21fe79b029d8afd1781836517b6bc2c7835705
AndrewGraber <graber15@purdue.edu>
2022-02-02 12:42:44 -0500

    Trying to get SSH keys to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import PageHolder from './components/PageHolder';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   	return (
